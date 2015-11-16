b5d4022ae5b90e25c275ff8313a89756e4117e88
goetzingert <mail@simplydevelop.de>
2015-11-16 20:52:26 +0100

    download path

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,7 @@ default[:flink][:version]                          = "0.10.0"
 default[:flink][:hadoop_version]                   = "27"
 default[:flink][:dir]                              = "/usr/local"
 default[:flink][:home]                             = "#{node[:flink][:dir]}/flink-#{node[:flink][:version]}"
-default[:flink][:url]                              = "http://apache.mirrors.spacedump.net/flink/flink-#{node[:flink][:version]}/flink-#{node[:flink][:version]}-bin-hadoop24.tgz"
+default[:flink][:url]                              = "http://apache.mirrors.spacedump.net/flink/flink-#{node[:flink][:version]}/flink-#{node[:flink][:version]}-bin-hadoop24-scala_2.11.tgz"
 default[:flink][:conf_dir]                         = "#{node[:flink][:home]}/conf"
 
 default[:flink][:checksum]                         = ""
